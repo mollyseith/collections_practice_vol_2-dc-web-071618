29ccadb64fa77df72ae7d5712c79d4f6b2f69b7b
Molly Seither <molly.seither@centre.edu>
2018-07-14 21:26:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,66 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |el|
+    el.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  as = []
+  array.collect do |el|
+    if el.include?("a")
+      as << el
+    end
+  end
+  as
+end
+
+def first_wa(array)
+  array.find do |el|
+    if el[0] == "w" && el[1] == "a"
+      el
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |el|
+    el.class != String
+  end
+  array
+end
+
+def count_elements(array)
+  array.group_by(&:itself).map do |key, value|
+    key.merge({:count => value.length})
+  end
+end
+
+def merge_data(keys, data)
+  new_keys_1 = {keys[0].keys.to_a[0] => data.to_a[0].to_a[0][0]}
+  new_data_1 = new_keys_1.merge(data[0].values.to_a[0])
+  new_keys_2 = {keys[1].keys.to_a[0] => data.to_a[0].to_a[1][0]}
+  new_data_2 = new_keys_2.merge(data[0].values.to_a[1])
+ [new_data_1, new_data_2]
+end
+
+def find_cool(cool)
+  if cool[1].values[1] == "cool"
+    [cool[1]]
+  end
+end
+
+def organize_schools(schools)
+  ret = []
+  schools.values.to_a.each do |value|
+    ret << value.values
+  end
+  school_array = ret.flatten.uniq.sort
+  h1 = {schools.to_a[0][1].values[0] => [schools.to_a[0][0], schools.to_a[1][0], schools.to_a[4][0]]}
+  h2 = {school_array[0] => [schools.to_a[3][0]]}
+  h3 = {school_array[2] => [schools.to_a[2][0], schools.to_a[5][0]]}
+  h = {}
+  h.merge!(h1)
+  h.merge!(h2)
+  h.merge!(h3)
+  h
+end
