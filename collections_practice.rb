51da466d119ac79caa0f062ed8b4357b6a6427cf
Molly Seither <molly.seither@centre.edu>
2018-07-14 20:54:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,21 +1,5 @@
-def begins_with_r(array)
-  i=0
-  while i < array.length
-    all = array.all?
-    if array[i].start_with?("r")
-      puts true
-      i+=1
-    else
-      puts false
-      i+=1
-    end
-  end
-end
-
-def begins_with_r(element)
-  if element.start_with?("r")
-    true
-  else
-    false
+def begins_with_r(tools)
+  tools.all? do |tool|
+    tool.start_with?("r")
   end
 end
